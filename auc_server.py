6702f1e8c4c5fbecdf7a7000a5941e443ca52d77
Kabir Singh Bhatia <kabirsinghbhatia@gmail.com>
2024-10-15 19:13:05 -0400

    server improvements, client testing file

--- auc_server.py
+++ auc_server.py
@@ -32,6 +32,9 @@ class AuctioneerServer:
                     conn.sendall(b"Server is busy. Try to connect again later.\n")
                     conn.close()
             elif self.status == 1:
+                if not self.auction_details:
+                    conn.sendall(b"Server is busy. Try to connect again later.\n")
+                    conn.close()
                 print(f"New Buyer is connected from {addr[0]}:{addr[1]}")
                 threading.Thread(target=self.handle_buyer, args=(conn, addr)).start()              
 
@@ -40,6 +43,7 @@ class AuctioneerServer:
         print(">> New Seller Thread spawned")
         self.seller_conn = conn
         conn.sendall(b"Your role is: [Seller]\nPlease submit auction request:\n")
+        self.status = 1
 
         while True:
             try:
@@ -58,9 +62,10 @@ class AuctioneerServer:
                         'auc_min_price': int(auc_min_price),
                         'max_bids': int(max_bids),
                         'item_name': str(item_name)
-                    }
-                    self.status = 1     
-                    print("Action request received. Now waiting for Buyer")               
+                    }     
+                    print(self.auction_details) 
+                    print("Action request received. Now waiting for Buyer")
+                                  
                     break
                 else:
                     raise Exception()
@@ -69,30 +74,35 @@ class AuctioneerServer:
                 conn.sendall(b"Server: Invalid auction request!\n")
                 continue
         
-        conn.close()
                 
 
 
     def handle_buyer(self, conn, addr):
         print(">> New Buyer Thread spawned")
+        conn.sendall(b"Your role is: [Buyer]\nThe Auctioneer is still waiting for other Buyer to connect...\n")
 
         with self.buyer_lock:
             if len(self.buyers) < self.auction_details['max_bids']:
                 buyer_number = len(self.buyers) + 1
                 buyer_id = f"Buyer {buyer_number}"
                 self.buyers.append((conn, buyer_id))
+                print(self.buyers)
             
                 if len(self.buyers) == self.auction_details['max_bids']:
+                    for conn, _, in self.buyers:
+                        conn.sendall(b"Requested number of bidders arrived. Let's start bidding!\n")
+                    print("Requested number of bidders arrived. Let's start bidding!")
                     self.start_bidding()
                 else:
                     conn.sendall(b"The Auctioneer is still waiting for other Buyer to connect...\n")
+                    print(f"Buyer len = {len(self.buyers)}")
             else:
                 conn.sendall(b"Server is busy. Try to connect again later.\n")
+                print(f"Buyer len = {len(self.buyers)}")
                 conn.close()
 
 
     def start_bidding(self):
-        print("Requested number of bidders arrived. Let's start bidding!")
 
         threads = []
         for conn, buyer_id in self.buyers:
@@ -108,33 +118,35 @@ class AuctioneerServer:
 
     def receive_bid(self, conn, buyer_id):
         while True:
+            conn.sendall(b"Please submit your bid:")
             data = conn.recv(1024).decode()
-            if not data:
-                break
-
-            try:
-                bid_amount = int(data)
-                with self.buyer_lock:
-                    self.bids[buyer_id] = bid_amount
-                    print(f"{buyer_id} bid ${bid_amount}")
-                    conn.sendall(b"Bid receive. Please wait...\n")
-                    break
-            except ValueError:
-                conn.sendall(b"Invalid bid.\n")
+            if data:
+                try:
+                    bid_amount = int(data)
+                    print(f"bid: {bid_amount}")
+                    with self.buyer_lock:
+                        print("Lock acquired")
+                        self.bids[buyer_id] = bid_amount
+                        print(f"{buyer_id} bid ${bid_amount}")
+                        conn.sendall(b"Bid receive. Please wait...\n")
+                        break
+                except ValueError:
+                    conn.sendall(b"Invalid bid.\n")
 
 
     def determine_winner(self):
-        highest_bidder_id = max(self.bids, key=self.bids.get)
-        highest_bid = self.bids[highest_bidder_id]
+        with self.buyer_lock:
+            highest_bidder_id = max(self.bids, key=self.bids.get)
+            highest_bid = self.bids[highest_bidder_id]
 
-        if highest_bid >= self.auction_details['auc_min_price']:
-            if self.auction_details['auc_type'] == 1:
-                self.notify_winner(highest_bidder_id, highest_bid)
-            elif self.auction_details['auc_type'] == 2:
-                second_highest_bid = sorted(self.bids.values(), reverse=True)[1]
-                self.notify_winner(highest_bidder_id, second_highest_bid)
-        else:
-            self.notify_no_sale()
+            if highest_bid >= self.auction_details['auc_min_price']:
+                if self.auction_details['auc_type'] == 1:
+                    self.notify_winner(highest_bidder_id, highest_bid)
+                elif self.auction_details['auc_type'] == 2:
+                    second_highest_bid = sorted(self.bids.values(), reverse=True)[1]
+                    self.notify_winner(highest_bidder_id, second_highest_bid)
+            else:
+                self.notify_no_sale()
 
     def notify_winner(self, winner_id, price):
         winner_conn = next(conn for conn, buyer_id in self.buyers if buyer_id == winner_id)
@@ -159,12 +171,15 @@ class AuctioneerServer:
     def reset_server(self):
         self.status = 0
         self.auction_details = None
-        self.seller_conn.close()
+
+        if self.seller_conn:
+            self.seller_conn.close()
         self.seller_conn = None
         for conn, _ in self.buyers:
             conn.close()
-        self.buyers.clear()
-        self.bids.clear()
+        with self.buyer_lock:
+            self.buyers.clear()
+            self.bids.clear()
 
 
 if __name__ == "__main__":
