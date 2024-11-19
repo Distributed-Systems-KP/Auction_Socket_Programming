9a79813e405c0f0242b823f7af56974db46ffce0
Prabhudatta Mishra <84654664+Prabhudatta3004@users.noreply.github.com>
2024-11-19 17:28:02 -0500

    Update auc_client_rdt.py

--- auc_client_rdt.py
+++ auc_client_rdt.py
@@ -322,7 +322,8 @@ def handle_file_receive(seller_ip, rdtport, packet_loss_rate=0.0):
                     print("Received end of transmission signal. Sent fin/ack")
                     end_time = time.time()
                     break
-            
+
+            expected_seq_num = 1
             if response_message['TYPE'] == 1:
                 
                 seq_num = response_message['SEQ/ACK']
